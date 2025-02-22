88cf8c47f4ed9b7caa58eef40a4ab93fd23f9a5a
Simon Melichar <dalsiemail7@seznam.cz>
2025-02-22 16:15:15 +0100

    cau

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -5,22 +5,19 @@ import "../styles/Footer.css";
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footer-container">
-        {/* Střed - EasyRent (Nyní uprostřed) */}
+      <div className="footer-container">       
         <div className="footer-center">
           <h3>EasyRent</h3>
           <p>Výběr aut</p>
           <p>O nás</p>
           <p>Kontaktovat</p>
         </div>
-
-        {/* Pravá část - Kontakt + Ikony */}
+        
         <div className="footer-right">
           <h3>Kontaktovat</h3>
           <p>info@easyrent.cz</p>
           <p>+420 123 456 789</p>
-
-          {/* Ikony pod kontaktem */}
+          
           <div className="footer-icons">
             <FaInstagram className="footer-icon" />
             <FaTwitter className="footer-icon" />
@@ -28,7 +25,7 @@ const Footer = () => {
         </div>
       </div>
 
-      {/* Copyright na střed */}
+     
       <p className="footer-bottom">© 2025 EasyRent.cz s.r.o</p>
     </footer>
   );
